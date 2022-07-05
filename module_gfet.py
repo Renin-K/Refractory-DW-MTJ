9b0081dc5f8f3bea2a67e92ec55e6b397341aaaa
Sam <liukts@utexas.edu>
2022-07-04 22:09:56 -0500

    gfet test

---
+++ module_gfet.py
@@ -0,0 +1,246 @@
+import torch
+import torch.jit
+import math
+import norse
+try:
+    import norse_op
+except ModuleNotFoundError:  # pragma: no cover
+    pass
+
+from norse.torch.module.snn import SNNCell
+from typing import NamedTuple, Optional, Tuple
+from norse.torch.functional.threshold import threshold
+
+class GFETParameters(NamedTuple):
+    """Parametrization of a LIF neuron
+
+    Parameters:
+        tau_syn_inv (torch.Tensor): inverse synaptic time
+                                    constant (:math:`1/\\tau_\\text{syn}`) in 1/ms
+        tau_mem_inv (torch.Tensor): inverse membrane time
+                                    constant (:math:`1/\\tau_\\text{mem}`) in 1/ms
+        v_leak (torch.Tensor): leak potential in mV
+        v_th (torch.Tensor): threshold potential in mV
+        v_reset (torch.Tensor): reset potential in mV
+        method (str): method to determine the spike threshold
+                      (relevant for surrogate gradients)
+        alpha (float): hyper parameter to use in surrogate gradient computation
+    """
+
+    tau_syn_inv: torch.Tensor = torch.as_tensor(1.0 / 5e-3)
+    tau_mem_inv: torch.Tensor = torch.as_tensor(1.0 / 1e-2)
+    v_leak: torch.Tensor = torch.as_tensor(0.0)
+    v_th: torch.Tensor = torch.as_tensor(1.0)
+    v_reset: torch.Tensor = torch.as_tensor(0.0)
+    method: str = "super"
+    alpha: float = torch.as_tensor(100.0)
+
+class GFETFeedForwardState(NamedTuple):
+    """State of a feed forward LIF neuron
+
+    Parameters:
+        v (torch.Tensor): membrane potential
+        i (torch.Tensor): synaptic input current
+    """
+
+    v: torch.Tensor
+    i: torch.Tensor
+
+class GFETCell(SNNCell):
+    """Module that computes a single euler-integration step of a
+    leaky integrate-and-fire (LIF) neuron-model *without* recurrence and *without* time.
+
+    More specifically it implements one integration step
+    of the following ODE
+
+    .. math::
+        \\begin{align*}
+            \\dot{v} &= 1/\\tau_{\\text{mem}} (v_{\\text{leak}} - v + i) \\
+            \\dot{i} &= -1/\\tau_{\\text{syn}} i
+        \\end{align*}
+
+    together with the jump condition
+
+    .. math::
+        z = \\Theta(v - v_{\\text{th}})
+
+    and transition equations
+
+    .. math::
+        \\begin{align*}
+            v &= (1-z) v + z v_{\\text{reset}}
+        \\end{align*}
+
+    Example:
+        >>> data = torch.zeros(5, 2) # 5 batches, 2 neurons
+        >>> l = LIFCell(2, 4)
+        >>> l(data) # Returns tuple of (Tensor(5, 4), LIFState)
+
+    Arguments:
+        p (LIFParameters): Parameters of the LIF neuron model.
+        sparse (bool): Whether to apply sparse activation functions (True) or not (False). Defaults to False.
+        dt (float): Time step to use. Defaults to 0.001.
+    """
+
+    def __init__(self, p: GFETParameters = GFETParameters(), **kwargs):
+        super().__init__(
+            activation=gfet_feed_forward_step,
+            activation_sparse= gfet_feed_forward_step_sparse,
+            state_fallback=self.initial_state,
+            p=GFETParameters(
+                torch.as_tensor(p.tau_syn_inv),
+                torch.as_tensor(p.tau_mem_inv),
+                torch.as_tensor(p.v_leak),
+                torch.as_tensor(p.v_th),
+                torch.as_tensor(p.v_reset),
+                p.method,
+                torch.as_tensor(p.alpha),
+            ),
+            **kwargs,
+        )
+
+    def initial_state(self, input_tensor: torch.Tensor) -> GFETFeedForwardState:
+        state = GFETFeedForwardState(
+            v=torch.full(
+                input_tensor.shape,
+                torch.as_tensor(self.p.v_leak).detach(),
+                device=input_tensor.device,
+                dtype=torch.float32,
+            ),
+            i=torch.zeros(
+                *input_tensor.shape,
+                device=input_tensor.device,
+                dtype=torch.float32,
+            ),
+        )
+        state.v.requires_grad = True
+        return state
+
+class GFETParametersJIT(NamedTuple):
+    """Parametrization of a LIF neuron
+
+    Parameters:
+        tau_syn_inv (torch.Tensor): inverse synaptic time
+                                    constant (:math:`1/\\tau_\\text{syn}`) in 1/ms
+        tau_mem_inv (torch.Tensor): inverse membrane time
+                                    constant (:math:`1/\\tau_\\text{mem}`) in 1/ms
+        v_leak (torch.Tensor): leak potential in mV
+        v_th (torch.Tensor): threshold potential in mV
+        v_reset (torch.Tensor): reset potential in mV
+        method (str): method to determine the spike threshold
+                      (relevant for surrogate gradients)
+        alpha (torch.Tensor): hyper parameter to use in surrogate gradient computation
+    """
+
+    tau_syn_inv: torch.Tensor
+    tau_mem_inv: torch.Tensor
+    v_leak: torch.Tensor
+    v_th: torch.Tensor
+    v_reset: torch.Tensor
+    method: str
+    alpha: torch.Tensor
+
+@torch.jit.script
+def _gfet_feed_forward_step_jit(
+    input_tensor: torch.Tensor,
+    state: GFETFeedForwardState,
+    p: GFETParametersJIT,
+    dt: float = 0.001,
+) -> Tuple[torch.Tensor, GFETFeedForwardState]:  # pragma: no cover
+    # compute voltage updates
+    dv = dt * p.tau_mem_inv * ((p.v_leak - state.v) + state.i)
+    v_decayed = state.v + dv
+    # torch.where(v_decayed > 0, v_decayed, torch.tensor(0,dtype=torch.float32,
+    #                                               device=torch.device("cuda")))
+
+    # compute current updates
+    di = -dt * p.tau_syn_inv * state.i
+    i_decayed = state.i + di
+
+    # compute new spikes
+    z_new = threshold(v_decayed - p.v_th, p.method, p.alpha)
+    # compute reset
+    v_new = (1 - z_new) * v_decayed + z_new * p.v_reset
+    # compute current jumps
+    i_new = i_decayed + input_tensor
+
+    return z_new, GFETFeedForwardState(v=v_new, i=i_new)
+
+def gfet_feed_forward_step(
+    input_tensor: torch.Tensor,
+    state: Optional[GFETFeedForwardState],
+    p: GFETParameters = GFETParameters(),
+    dt: float = 0.001,
+) -> Tuple[torch.Tensor, GFETFeedForwardState]:
+    r"""Computes a single euler-integration step for a lif neuron-model.
+    It takes as input the input current as generated by an arbitrary torch
+    module or function. More specifically it implements one integration
+    step of the following ODE
+
+    .. math::
+        \begin{align*}
+            \dot{v} &= 1/\tau_{\text{mem}} (v_{\text{leak}} - v + i) \\
+            \dot{i} &= -1/\tau_{\text{syn}} i
+        \end{align*}
+
+    together with the jump condition
+
+    .. math::
+        z = \Theta(v - v_{\text{th}})
+
+    and transition equations
+
+    .. math::
+        \begin{align*}
+            v &= (1-z) v + z v_{\text{reset}} \\
+            i &= i + i_{\text{in}}
+        \end{align*}
+
+    where :math:`i_{\text{in}}` is meant to be the result of applying an
+    arbitrary pytorch module (such as a convolution) to input spikes.
+
+    Parameters:
+        input_tensor (torch.Tensor): the input spikes at the current time step
+        state (LIFFeedForwardState): current state of the LIF neuron
+        p (LIFParameters): parameters of a leaky integrate and fire neuron
+        dt (float): Integration timestep to use
+    """
+    if norse.utils.IS_OPS_LOADED:
+        try:
+            z, v, i = norse_op.lif_super_feed_forward_step(input_tensor, state, p, dt)
+            return z, GFETFeedForwardState(v=v, i=i)
+        except NameError:  # pragma: no cover
+            pass
+    jit_params = GFETParametersJIT(
+        tau_syn_inv=p.tau_syn_inv,
+        tau_mem_inv=p.tau_mem_inv,
+        v_leak=p.v_leak,
+        v_th=p.v_th,
+        v_reset=p.v_reset,
+        method=p.method,
+        alpha=torch.as_tensor(p.alpha),
+    )
+    return _gfet_feed_forward_step_jit(input_tensor, state=state, p=jit_params, dt=dt)
+
+def gfet_feed_forward_step_sparse(
+    input_tensor: torch.Tensor,
+    state: GFETFeedForwardState,
+    p: GFETParameters,
+    dt: float = 0.001,
+) -> Tuple[torch.Tensor, GFETFeedForwardState]:  # pragma: no cover
+    # compute voltage updates
+    dv = dt * p.tau_mem_inv * ((p.v_leak - state.v) + state.i)
+    v_decayed = state.v + dv
+
+    # compute current updates
+    di = -dt * p.tau_syn_inv * state.i
+    i_decayed = state.i + di
+
+    # compute new spikes
+    z_new = threshold(v_decayed - p.v_th, p.method, p.alpha)
+    # compute reset
+    v_new = (1 - z_new) * v_decayed + z_new * p.v_reset
+    # compute current jumps
+    i_new = i_decayed + input_tensor
+
+    return z_new.to_sparse(), GFETFeedForwardState(v=v_new, i=i_new)
