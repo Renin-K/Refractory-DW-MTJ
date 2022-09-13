c14c17b8375cb44669bb194b52a821c47b74dfd9
Sam <liukts@utexas.edu>
2022-09-13 03:55:34 -0500

    new mod

---
+++ fashion_stoch.py
@@ -0,0 +1,197 @@
+import torch
+import numpy as np
+import torchvision
+import os
+
+from module_stochastic import StochParameters
+from module_stochastic import StochCell
+
+from norse.torch.module.leaky_integrator import LILinearCell
+from norse.torch.module import encode
+
+from tqdm import tqdm, trange
+
+# folder to save results
+target_dir = "0912_fashion_default"
+
+BATCH_SIZE = 100
+
+transform = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+    ]
+)
+
+train_data = torchvision.datasets.FashionMNIST(
+    root=".",
+    train=True,
+    download=True,
+    transform=transform,
+)
+
+train_loader = torch.utils.data.DataLoader(
+    train_data,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.FashionMNIST(
+        root=".",
+        train=False,
+        transform=transform,
+    ),
+    batch_size=BATCH_SIZE
+)
+
+def decode(x):
+    x, _ = torch.max(x, 0)
+    log_p_y = torch.nn.functional.log_softmax(x, dim=1)
+    return log_p_y
+
+class ConvNet(torch.nn.Module):
+    def __init__(
+        self,  num_channels=1, feature_size=28, beta=1, method="super", alpha=100
+    ):
+        super(ConvNet, self).__init__()
+
+        self.features = int(((feature_size - 4) / 2 - 4) / 2)
+
+        self.conv1 = torch.nn.Conv2d(num_channels, 20, 5, 1)
+        self.conv2 = torch.nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = torch.nn.Linear(self.features * self.features * 50, 500)
+        self.stoch0 = StochCell(p=StochParameters(beta=beta,method=method, alpha=alpha))
+        self.stoch1 = StochCell(p=StochParameters(beta=beta,method=method, alpha=alpha))
+        self.stoch2 = StochCell(p=StochParameters(beta=beta,method=method, alpha=alpha))
+        self.out = LILinearCell(500, 10)
+
+    def forward(self, x):
+        seq_length = x.shape[0]
+        batch_size = x.shape[1]
+        
+        # specify the initial states
+        s0 = s1 = s2 = so = None
+
+        voltages = torch.zeros(
+            seq_length, batch_size, 10, device=x.device, dtype=x.dtype
+        )
+
+        for ts in range(seq_length):
+            z = self.conv1(x[ts, :])
+            z, s0 = self.stoch0(z, s0)
+            z = torch.nn.functional.max_pool2d(z, 2, 2)
+            z = 10 * self.conv2(z)
+            z, s1 = self.stoch1(z, s1)
+            z = torch.nn.functional.max_pool2d(z, 2, 2)
+            z = z.view(-1, 4 ** 2 * 50)
+            z = self.fc1(z)        
+            z, s2 = self.stoch2(z, s2)
+            v, so = self.out(torch.nn.functional.relu(z), so)
+            voltages[ts, :, :] = v
+        return voltages
+
+class Model(torch.nn.Module):
+    def __init__(self, encoder, snn, decoder):
+        super(Model, self).__init__()
+        self.encoder = encoder
+        self.snn = snn
+        self.decoder = decoder
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.snn(x)
+        log_p_y = self.decoder(x)
+        return log_p_y
+
+def train(model, device, train_loader, optimizer, epoch, max_epochs):
+    model.train()
+    losses = []
+    for (data, target) in tqdm(train_loader, desc='train', unit='batch', ncols=80, leave=False):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = torch.nn.functional.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+
+    mean_loss = np.mean(losses)
+    return losses, mean_loss
+
+def test(model, device, test_loader, epoch):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in tqdm(test_loader, desc='test', unit='batch', ncols=80, leave=False):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += torch.nn.functional.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100.0 * correct / len(test_loader.dataset)
+    return test_loss, accuracy
+
+def save(path, epoch, model, optimizer, is_best=False):
+    torch.save(
+        {
+            "epoch": epoch + 1,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+        },
+        path,
+    )
+
+EPOCHS = 5  
+T = 48
+LR = 0.001
+
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device("cpu")
+
+model = Model(
+    encoder=encode.ConstantCurrentLIFEncoder(T),
+    snn=ConvNet(alpha=80),
+    decoder=decode
+).to(DEVICE)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+training_losses = []
+mean_losses = []
+test_losses = []
+accuracies = []
+
+pbar = trange(EPOCHS, ncols=80, unit="epoch")
+for epoch in pbar:
+    training_loss, mean_loss = train(model, DEVICE, train_loader, optimizer, epoch, max_epochs=EPOCHS)
+    test_loss, accuracy = test(model, DEVICE, test_loader, epoch)
+    training_losses += training_loss
+    mean_losses.append(mean_loss)
+    test_losses.append(test_loss)
+    accuracies.append(accuracy)       
+    pbar.set_postfix(accuracy=accuracies[-1])
+
+os.mkdir("./outputs/" + target_dir)
+np.save("./outputs/" + target_dir + "/training_losses.npy", np.array(training_losses))
+np.save("./outputs/" + target_dir + "/mean_losses.npy", np.array(mean_losses))
+np.save("./outputs/" + target_dir + "/test_losses.npy", np.array(test_losses))
+np.save("./outputs/" + target_dir + "/accuracies.npy", np.array(accuracies))
+model_path = "./outputs/" + target_dir + "/fmnist_dwmtj.pt"
+save(
+    model_path,
+    epoch=epoch,
+    model=model,
+    optimizer=optimizer,
+)
+
+print(f"final accuracy: {accuracies[-1]}")
---
+++ module_stochastic.py
@@ -0,0 +1,155 @@
+import torch
+import torch.jit
+import math
+import norse
+
+from norse.torch.module.snn import SNNCell
+from typing import NamedTuple, Optional, Tuple
+from norse.torch.functional.threshold import threshold
+
+class StochParameters(NamedTuple):
+    """Parametrization of a LIF neuron
+
+    Parameters:
+        tau_syn_inv (torch.Tensor): inverse synaptic time
+                                    constant (:math:`1/\\tau_\\text{syn}`) in 1/ms
+        tau_mem_inv (torch.Tensor): inverse membrane time
+                                    constant (:math:`1/\\tau_\\text{mem}`) in 1/ms
+        v_leak (torch.Tensor): leak potential in mV
+        v_th (torch.Tensor): threshold potential in mV
+        v_reset (torch.Tensor): reset potential in mV
+        method (str): method to determine the spike threshold
+                      (relevant for surrogate gradients)
+        alpha (float): hyper parameter to use in surrogate gradient computation
+    """
+
+    beta: float = torch.as_tensor(1)
+    method: str = "super"
+    alpha: float = torch.as_tensor(100.0)
+
+class StochFeedForwardState(NamedTuple):
+    """State of a feed forward LIF neuron
+
+    Parameters:
+        i (torch.Tensor): synaptic input current
+    """
+    v: torch.Tensor
+    i: torch.Tensor
+
+class StochCell(SNNCell):
+    """Module that computes a single euler-integration step of a
+    leaky integrate-and-fire (LIF) neuron-model *without* recurrence and *without* time.
+
+    More specifically it implements one integration step
+    of the following ODE
+
+    .. math::
+        \\begin{align*}
+            \\dot{v} &= 1/\\tau_{\\text{mem}} (v_{\\text{leak}} - v + i) \\
+            \\dot{i} &= -1/\\tau_{\\text{syn}} i
+        \\end{align*}
+
+    together with the jump condition
+
+    .. math::
+        z = \\Theta(v - v_{\\text{th}})
+
+    and transition equations
+
+    .. math::
+        \\begin{align*}
+            v &= (1-z) v + z v_{\\text{reset}}
+        \\end{align*}
+
+    Example:
+        >>> data = torch.zeros(5, 2) # 5 batches, 2 neurons
+        >>> l = LIFCell(2, 4)
+        >>> l(data) # Returns tuple of (Tensor(5, 4), LIFState)
+
+    Arguments:
+        p (LIFParameters): Parameters of the LIF neuron model.
+        sparse (bool): Whether to apply sparse activation functions (True) or not (False). Defaults to False.
+        dt (float): Time step to use. Defaults to 0.001.
+    """
+
+    def __init__(self, p: StochParameters = StochParameters(), **kwargs):
+        super().__init__(
+            activation=stoch_feed_forward_step,
+            activation_sparse= stoch_feed_forward_step_sparse,
+            state_fallback=self.initial_state,
+            p=StochParameters(
+                torch.as_tensor(p.beta),
+                p.method,
+                torch.as_tensor(p.alpha),
+            ),
+            **kwargs,
+        )
+
+    def initial_state(self, input_tensor: torch.Tensor) -> StochFeedForwardState:
+        state = StochFeedForwardState(
+            i=torch.zeros(
+                *input_tensor.shape,
+                device=input_tensor.device,
+                dtype=torch.float32,
+            ),
+        )
+        state.v.requires_grad = True
+        return state
+
+class StochParametersJIT(NamedTuple):
+    """Parametrization of a LIF neuron
+
+    Parameters:
+        tau_syn_inv (torch.Tensor): inverse synaptic time
+                                    constant (:math:`1/\\tau_\\text{syn}`) in 1/ms
+        tau_mem_inv (torch.Tensor): inverse membrane time
+                                    constant (:math:`1/\\tau_\\text{mem}`) in 1/ms
+        v_leak (torch.Tensor): leak potential in mV
+        v_th (torch.Tensor): threshold potential in mV
+        v_reset (torch.Tensor): reset potential in mV
+        method (str): method to determine the spike threshold
+                      (relevant for surrogate gradients)
+        alpha (torch.Tensor): hyper parameter to use in surrogate gradient computation
+    """
+
+    beta: torch.Tensor
+    method: str
+    alpha: torch.Tensor
+
+@torch.jit.script
+def _stoch_feed_forward_step_jit(
+    input_tensor: torch.Tensor,
+    state: StochFeedForwardState,
+    p: StochParametersJIT,
+    dt: float = 0.001,
+) -> Tuple[torch.Tensor, StochFeedForwardState]:  # pragma: no cover
+    # compute voltage updates
+
+    z_new = torch.sigmoid(p.beta*input_tensor)
+
+    return z_new, StochFeedForwardState(v=state.v,i=state.i)
+
+def stoch_feed_forward_step(
+    input_tensor: torch.Tensor,
+    state: Optional[StochFeedForwardState],
+    p: StochParameters = StochParameters(),
+    dt: float = 0.001,
+) -> Tuple[torch.Tensor, StochFeedForwardState]:
+
+    jit_params = StochParametersJIT(
+        beta = p.beta,
+        method=p.method,
+        alpha=torch.as_tensor(p.alpha),
+    )
+    return _stoch_feed_forward_step_jit(input_tensor, state=state, p=jit_params, dt=dt)
+
+def stoch_feed_forward_step_sparse(
+    input_tensor: torch.Tensor,
+    state: StochFeedForwardState,
+    p: StochParameters,
+    dt: float = 0.001,
+) -> Tuple[torch.Tensor, StochFeedForwardState]:  # pragma: no cover
+
+    z_new = torch.sigmoid(p.beta*input_tensor)
+
+    return z_new.to_sparse, StochFeedForwardState(v=state.v,i=state.i)
